41cd313e3c494f056fca07bb26298ad74c4069e9
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-10 16:02:03 +0800

    add function for saving random walks

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,14 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#include <fstream>
+#include <string>
 #include <algorithm>
 #include <vector>
 using namespace std;
 
 bool cmp(const vector<double> a, const vector<double> b);
 double dist_func(double a, double b);
+void SaveRandomWalks(vector< vector<long> > walks);
 
 #endif
