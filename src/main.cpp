9e9c33a10e7f70072c1a2810d6279d596277e056
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-10 16:04:15 +0800

    add random seed and remove debug print

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <cstdlib>
+#include <ctime>
 #include "graph.h"
-//#include "distance_alg.h"
 #include "struc2vec.h"
 using namespace std;
 
 int main(){
+    srand(time(NULL));
     string filename = "barbell.txt";
     Graph G(filename, false);
 
@@ -14,55 +16,11 @@ int main(){
 
     struc2vec s(G, true, 3);
 
-    cout << "PreprocessNeighborsBFS" << endl;
-    s.PreprocessNeighborsBFS();
-    cout << "PreprocessNeighborsBFS" << endl;
-    cout << "PreprocessDegreeLists" << endl;
-    s.PreprocessDegreeLists();
-    cout << "PreprocessDegreeLists" << endl;
-    cout << "CreateVectors" << endl;
-    s.CreateVectors();
-    cout << "CreateVectors" << endl;
-    cout << "CalDistAllVertices" << endl;
-    s.CalDistAllVertices();
-    cout << "CalDistAllVertices" << endl;
-    cout << "CalDistVertices" << endl;
-    s.CalDistVertices();
-    cout << "CalDistVertices" << endl;
-    cout << "CreateDistNetwork" << endl;
     s.CreateDistNetwork();
-    cout << "CreateDistNetwork" << endl;
-    cout << "PreprocessParamsRandomWalk" << endl;
     s.PreprocessParamsRandomWalk();
-    cout << "PreprocessParamsRandomWalk" << endl;
-    cout << "SimulateWalks" << endl;
-    s.SimulateWalks(10, 80);
-    cout << "SimulateWalks" << endl;
-/*
-    map< long, vector<long> > m = G.getGraph();
-    map< long, map< int, vector<double> > > degree_lists = getDegreeListsVertices(G, 3);
+    vector< vector<long> > walks = s.SimulateWalks(10, 80);
+    
+    SaveRandomWalks(walks);
 
-    for(auto& v: degree_lists){
-        cout << v.first << endl;
-
-        for(auto& l: v.second){
-            cout << l.first << ":";
-
-            for(auto e: l.second){
-                cout << " " << e;
-            }
-            cout << endl;
-        }
-
-        cout << endl;
-    }
-
-    cout << structuralDist(degree_lists[18], degree_lists[19], 1) << endl;
-    cout << structuralDist(degree_lists[18], degree_lists[19], 2) << endl;
-    cout << structuralDist(degree_lists[18], degree_lists[19], 3) << endl;
-    cout << DTW(degree_lists[18][3], degree_lists[19][1]) << endl;
-    cout << DTW(degree_lists[18][3], degree_lists[19][2]) << endl;
-    cout << DTW(degree_lists[18][3], degree_lists[19][3]) << endl;
-*/
     return 0;
 }
