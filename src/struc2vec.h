9849312aad939d3d7bf6cd4e5a9f0eb4656db050
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-10 11:35:31 +0800

    remove unused function and library

--- src/struc2vec.h
+++ src/struc2vec.h
@@ -10,19 +10,8 @@
 #include <cmath>
 #include <climits>
 #include "graph.h"
-//#include "distance_alg.h"
 #include "utils.h"
 using namespace std;
-
-struct hash_pair {
-    template <class T1, class T2>
-    size_t operator()(const pair<T1, T2>& p) const
-    {
-        auto hash1 = hash<T1>{}(p.first);
-        auto hash2 = hash<T2>{}(p.second);
-        return hash1 ^ hash2;
-    }
-};
 
 class struc2vec{
     public:
