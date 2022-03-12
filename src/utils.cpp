8feca1fb1ad051000638db65d3cd1bf04b88ed4d
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-12 14:52:21 +0800

    add word2vec functions but have not successfully compiled

--- src/utils.cpp
+++ src/utils.cpp
@@ -26,3 +26,48 @@ void SaveRandomWalks(vector< vector<long> > walks, Graph G){
 
     f.close();
 }
+
+void word2vec_train(string train_filename, string model_filename, int dimensions) {
+    w2v::trainSettings_t trainSettings;
+
+    string stopWordsFile = "";
+
+    trainSettings.size = static_cast<uint16_t>(dimensions);
+    trainSettings.withSG = true;
+
+
+    w2v::w2vModel_t model;
+    bool trained = model.train(trainSettings, train_filename, stopWordsFile, nullptr, nullptr, nullptr);
+
+    if (!trained) {
+        cerr << "Training failed: " << model.errMsg() << endl;
+        return;
+    }
+
+    if (!model.save(model_filename)) {
+        cerr << "Model file saving failed: " << model.errMsg() << endl;
+        return;
+    }
+}
+
+void SaveEmbedding(string w2v_filename, string embed_filename){
+    // load pre-trained model
+    unique_ptr<w2v::w2vModel_t> w2vModel;
+    w2vModel.reset(new w2v::w2vModel_t());
+    w2vModel->load(w2v_filename);
+
+    w2v::word2vec_t GGG(w2vModel, "GGG");
+
+    ofstream f(embed_filename);
+
+    if(f.is_open()){
+        for(auto x: GGG){
+            f << x << " ";
+        }
+
+        f << endl;
+    }
+
+    f.close();
+}
+
--- src/utils.h
+++ src/utils.h
@@ -6,10 +6,13 @@
 #include <algorithm>
 #include <vector>
 #include "graph.h"
+#include "word2vec/include/word2vec.hpp"
 using namespace std;
 
 bool cmp(const vector<double> a, const vector<double> b);
 double dist_func(double a, double b);
 void SaveRandomWalks(vector< vector<long> > walks, Graph G);
+void word2vec_train(string train_filename, string model_filename, int dimensions);
+void SaveEmbedding(string w2v_filename, string embed_filename);
 
 #endif
