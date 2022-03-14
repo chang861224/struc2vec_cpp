47219eed7f567613967f24b6bdc1ef925c8bfb72
Chi-Hung Chang <chihung861224@gmail.com>
2022-03-14 15:41:48 +0800

    sample usage code with executing time

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 #include "src/graph.h"
 #include "src/struc2vec.h"
 using namespace std;
@@ -12,17 +13,28 @@ int main(){
     string filename = "graph/PubMed.edgelist";
     Graph G(filename, false);
 
-    cout << "Total edges: " << G.getNumEdges() << endl;
+    int layers = 3;
+    int num_walks = 10;
+    int walk_length = 80;
 
-    struc2vec s(G, true, 3);
+    clock_t start = clock();
 
-    s.CreateDistNetwork();
-    s.PreprocessParamsRandomWalk();
-    vector< vector<long> > walks = s.SimulateWalks(10, 80);
+    struc2vec model(G, true, layers);
+
+    model.CreateDistNetwork();
+    model.PreprocessParamsRandomWalk();
+    vector< vector<long> > walks = model.SimulateWalks(num_walks, walk_length);
     
     SaveRandomWalks(walks, G);
     word2vec_train("random_walks.txt", "w2v.model", 128);
     SaveEmbedding("w2v.model", "embedding.emb", G);
+
+    clock_t end = clock();
+
+    double duration = double(end - start) / double(CLOCKS_PER_SEC);
+
+    cout << "Total edges: " << G.getNumEdges() << endl;
+    cout << "Execution time: " << fixed << duration << setprecision(4) << " sec" << endl;
 
     return 0;
 }
